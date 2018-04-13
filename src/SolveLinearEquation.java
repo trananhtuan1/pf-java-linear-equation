db14b6c0fa02ce375137183e6a18f12ebf2e3d02
tuan <tincanxh@gmail.com>
2018-04-13 12:00:26 +0700

    get equation's arguments from keyboard

--- src/SolveLinearEquation.java
+++ src/SolveLinearEquation.java
@@ -1,5 +1,14 @@
+import java.util.Scanner;
+
 public class SolveLinearEquation {
     public static void main(String[] args) {
+        System.out.println("Please input arguments");
 
+        System.out.print("a: ");
+        double a = new Scanner(System.in).nextDouble();
+        System.out.print("b: ");
+        double b = new Scanner(System.in).nextDouble();
+
+        System.out.println("You inputed " + a + " and " + b);
     }
 }
