8032f7ff8091e160fea37d0d2de42c0fab19e094
tuan <tincanxh@gmail.com>
2018-04-13 12:27:46 +0700

    fix published methods

--- src/SolveLinearEquation.java
+++ src/SolveLinearEquation.java
@@ -12,25 +12,25 @@ public class SolveLinearEquation {
         System.out.println("Thanks you!");
     }
 
-    public static boolean askUserToContinue() {
+    private static boolean askUserToContinue() {
         System.out.println("Are you want to contine? (Y/n)");
         String answer = new Scanner(System.in).nextLine();
         boolean isUserAnswerYes = answer.equals("y") || answer.equals("Y");
         return isUserAnswerYes;
     }
 
-    public static double[] getArgumentsFromKeyboard() {
+    private static double[] getArgumentsFromKeyboard() {
         double a = printMessageAndGetDoubleFromKeyboard("a: ");
         double b = printMessageAndGetDoubleFromKeyboard("b: ");
         return new double[]{a, b};
     }
 
-    public static double printMessageAndGetDoubleFromKeyboard(String message) {
+    private static double printMessageAndGetDoubleFromKeyboard(String message) {
         System.out.print(message);
         return new Scanner(System.in).nextDouble();
     }
 
-    public static double[] solve(double a, double b) {
+    private static double[] solve(double a, double b) {
         boolean conghiem = a != 0;
         if (!conghiem) return new double[]{};
 
@@ -38,7 +38,7 @@ public class SolveLinearEquation {
         return new double[]{ngiem};
     }
 
-    public static void printTapNghiem(double a, double b, double[] tapnghiem) {
+    private static void printTapNghiem(double a, double b, double[] tapnghiem) {
         if (tapnghiem.length == 0) System.out.println("Phuong trinh "
                 + a + " * x + " + b + " = 0" +
                 " vo nghiem");
