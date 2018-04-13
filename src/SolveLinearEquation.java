98702f85beeb324a2fc47f33c6683a4d74d742b7
tuan <tincanxh@gmail.com>
2018-04-13 12:26:54 +0700

    refactor ask user method

--- src/SolveLinearEquation.java
+++ src/SolveLinearEquation.java
@@ -2,17 +2,12 @@ import java.util.Scanner;
 
 public class SolveLinearEquation {
     public static void main(String[] args) {
-        boolean wantToSolve = true;
-
-        while (wantToSolve) {
+        do {
             System.out.println("Please input arguments");
-
             double[] arguments = getArgumentsFromKeyboard();
             double[] tapnghiem = solve(arguments[0], arguments[1]);
             printTapNghiem(arguments[0], arguments[1], tapnghiem);
-
-            wantToSolve = askUserToContinue();
-        }
+        } while (askUserToContinue());
 
         System.out.println("Thanks you!");
     }
@@ -20,8 +15,8 @@ public class SolveLinearEquation {
     public static boolean askUserToContinue() {
         System.out.println("Are you want to contine? (Y/n)");
         String answer = new Scanner(System.in).nextLine();
-        if (answer.equals("y") || answer.equals("Y")) return true;
-        return false;
+        boolean isUserAnswerYes = answer.equals("y") || answer.equals("Y");
+        return isUserAnswerYes;
     }
 
     public static double[] getArgumentsFromKeyboard() {
